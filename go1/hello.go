02356e09879b8192562b29e6037fd72bf6f45512
mingquan.li <mingquan.li@5262.com>
2019-07-17 22:33:10 +0800

    hello world 解读

--- go1/hello.go
+++ go1/hello.go
@@ -1,7 +1,14 @@
+//main 包main函数是go程序执行的起点
 package main
 
+//不得导入程序中用不到的包 否则编译错误
 import "fmt"
 
+//main函数不能带参数 不能定义返回值
 func main() {
 	fmt.Println("hello world")
 }
+
+//直接运行 go run hello.go
+//编译成hello.exe go build hello.go
+//然后运行hello.exe
