a1fd3bf29a376e6f0011656c888e64af69f5d954
limq <limq@cgnfs.com.cn>
2019-07-24 08:54:05 +0800

    for range arr

--- gos8/arr3.go
+++ gos8/arr3.go
@@ -9,11 +9,23 @@ func main() {
 
 	fmt.Println(arr, a1, a2)
 
+	//使用for循环遍历数组
 	for i := 0; i < len(arr); i++ {
 		fmt.Println("Element ", i, "of arr is ", arr[i])
 	}
 
+	//range 遍历数组 方式1 key and value
 	for i, v := range arr {
 		fmt.Println("range i", i, "value", v)
+	}
+
+	//只要value
+	for _, v := range arr {
+		fmt.Println("range v :", v)
+	}
+
+	//只要key
+	for k := range arr {
+		fmt.Println("range only k :", k)
 	}
 }
