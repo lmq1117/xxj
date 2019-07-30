7a10d3a7830550fd9a2ca9ed1fb6e21886771157
limq <limq@cgnfs.com.cn>
2019-07-30 16:02:09 +0800

    为基本数据类型添加成员方法 通过func 和方法名 之间添加所属类型声明方式添加成员方法

---
+++ xxj/goo/3/int.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+type Integer int //对类型再定义
+//type Integer = int //别名
+
+func (a Integer) Equal(b Integer) bool {
+	return a == b
+}
+
+func (a Integer) LessThan(b Integer) bool {
+	return a < b
+}
+
+func (a Integer) MoreThan(b Integer) bool {
+	return a > b
+}
+
+func (a *Integer) Incr(i Integer) {
+	*a = *a + i
+}
+
+func (a *Integer) Decr(i Integer) {
+	*a = *a - i
+}
+
+func main() {
+	var a Integer = 5
+	var b Integer = 3
+	if a.Equal(b) {
+		fmt.Println("a eq b")
+	} else if a.LessThan(b) {
+		fmt.Println("a less than b")
+	} else if a.MoreThan(b) {
+		fmt.Println("a more than b")
+	}
+	a.Decr(2)
+	fmt.Println(a)
+	a.Incr(4)
+	fmt.Println(a)
+}
