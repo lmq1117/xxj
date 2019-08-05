9d1bb762e5c81b7a86b730293d4e05857372a98e
limq <limq@cgnfs.com.cn>
2019-08-05 09:55:04 +0800

    如何检测变量类型

--- go/11/3/assertion.go
+++ go/11/3/assertion.go
@@ -32,13 +32,26 @@ func main() {
 	sq1 := new(Square)
 	sq1.side = 5
 	areaIntf = sq1
-	if t, ok := areaIntf.(*Square); ok {
+
+	//if t, ok := areaIntf.(*Square); ok {
+	//	fmt.Printf("变量 areaIntf 的类型是 is %T\n", t)
+	//}
+	//if u, ok := areaIntf.(*Circle); ok {
+	//	fmt.Printf("变量 areaIntf 的类型是 is %T\n", u)
+	//} else {
+	//	fmt.Printf("变量 areaIntf 的类型不是Circle\n")
+	//}
+
+	//类型判断
+	switch t := areaIntf.(type) {
+	case *Circle:
+	case *Square:
 		fmt.Printf("变量 areaIntf 的类型是 is %T\n", t)
-	}
-	if u, ok := areaIntf.(*Circle); ok {
-		fmt.Printf("变量 areaIntf 的类型是 is %T\n", u)
-	} else {
-		fmt.Printf("变量 areaIntf 的类型不是Circle\n")
+	case nil:
+		fmt.Printf("nil value nothing to check?\n")
+	default:
+		fmt.Printf("unexcepted type %T\n", t)
+
 	}
 
 }
