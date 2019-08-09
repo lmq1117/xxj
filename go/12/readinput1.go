39b4aa12cb3a0f99e931736da3c16a2cb5ff682a
limq <limq@cgnfs.com.cn>
2019-08-09 14:46:42 +0800

    fmt.Scanln fmt.Scanf
    fmt.Sscanf

--- go/12/readinput1.go
+++ go/12/readinput1.go
@@ -11,11 +11,16 @@ var (
 )
 
 func main() {
-	fmt.Println("please input your full name: ")
-	fmt.Scanln(&firstName, &lastName)
-	fmt.Printf("Hi %s %s\n", firstName, lastName)
+	//从标准输入读取
+	//fmt.Println("please input your full name: ")
+	//fmt.Scanln(&firstName, &lastName)
+	//fmt.Printf("Hi %s %s\n", firstName, lastName)
 
-	//
+	//从标准输入读取
+	//ss,_ := fmt.Scanf("%s %s\n", &firstName, &lastName)
+	//fmt.Println(ss,firstName, lastName)
+
+	//从字符串读取
 	fmt.Sscanf(input, format, &f, &i, &s)
 	fmt.Println("from the string we read: ", f, i, s)
 }
